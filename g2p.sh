755a41e5471eb37cbdef2669ab44b38c428d6da5
Eduardo Silva <zedudu@gmail.com>
2016-02-22 19:29:39 -0300

    Fix g2p.sh interface

--- g2p.sh
+++ g2p.sh
@@ -1,57 +1,32 @@
 #!/bin/bash
 # create_lexicon.sh: generate lexicon file from a given word list file.
 
-help_message="usage: `basename $0` -w <input-words-list-file> -o <output-lexicon-file> [-s num_splits] [-l language]
+help_message="usage: `basename $0` -w <input-words-list-file> [-o <output-lexicon-file>] [-s num_splits] [-l language]
 
 Generate lexicon file from a given word list file.
 
 -w <input-words-list-file>  Mandatory: File with one word per line.
--o <output-lexicon-file>    Mandatory: Output file in which the lexicon will be saved.
+[-o <output-lexicon-file>]  Optional: Output file in which the lexicon will be saved. If it is not given, the output is sent to stdout.
 [-s number-of-splits]       Optional: If input words is too big (e.g. more than 10 million words),
                             it must be splitted to avoid espeak seg faults. By default,
                             number-of-splits is equal to 1.
 [-l language]               Optional: Defines the language used to build the lexicon. By
                             default, it is Brazilian Portuguese. But, the following languages
-                            may be used:
-                            af     Afrikaans
-                            bs     Bosnian
-                            ca     Catalan
-                            cs     Czech
-                            da     Danish
-                            de     German
-                            el     Greek
-                            en     Default English
-                            en-us  American English
-                            en-sc  Scottich English
-                            en-n   Northern British English
-                            en-rp  Received Pronunciation British English
-                            en-wm  West Midlands British English
-                            eo     Esperanto
-                            es     Spanish
-                            es-la  Spanish - Latin America
-                            fi     Finnish
-                            fr     French
-                            hr     Croatian
-                            hu     Hungarian
-                            it     Italian
-                            kn     Kannada
-                            ku     Kurdish
-                            lv     Latvian
-                            nl     Dutch
-                            pl     Polish
-                            pt     Portuguese (Brazil)
-                            pt-pt  Portuguese (European)
-                            ro     Romanian
-                            sk     Slovak
-                            sr     Serbian
-                            sv     Swedish
-                            sw     Swahihi
-                            ta     Tamil
-                            tr     Turkish
-                            zh     Mandarin Chinese
+                            may be used: 
+                            af (Afrikaans), bs (Bosnian), ca (Catalan), cs (Czech),
+                            da (Danish), de (German), el (Greek), en (Default English), 
+                            en-us (American English), en-sc (Scottich English), 
+                            en-n (Northern British English), en-rp (Received Pronunciation British English),
+                            en-wm (West Midlands British English), eo (Esperanto), es (Spanish),
+                            es-la (Spanish - Latin America), fi (Finnish), fr (French), hr (Croatian),
+                            hu (Hungarian), it (Italian), kn (Kannada), ku (Kurdish), lv (Latvian),
+                            nl (Dutch), pl (Polish), pt (Portuguese (Brazil)), pt-pt (Portuguese (European)),
+                            ro (Romanian), sk (Slovak), sr (Serbian), sv (Swedish), sw (Swahihi),
+                            ta (Tamil), tr (Turkish), zh (Mandarin Chinese)
 "
 
 SPLITS=1
+OUTFILE=/dev/stdout
 LANGUAGE=pt #by default we will use it for brazilian portuguese. 
             #It may be used for other languages. 
             #Just check espeak supported languages 
@@ -75,14 +50,15 @@ while getopts "h?w:o:s:l:" opt; do
     esac
 done
 
-if [ -z "$WORDSLIST" ] || [ -z "$OUTFILE" ] ; then
-  echo "aqui"
+if [ ! -f "$WORDSLIST" ]; then
+  echo "File \"$WORDSLIST\" does not exist."
   echo "$help_message" >&2
   exit 1
 fi
 
-
-echo "\"$WORDSLIST\" will be splitted in $SPLITS file(s)."
+if [ $OUTFILE != "/dev/stdout" ]; then
+  echo "\"$WORDSLIST\" will be splitted in $SPLITS file(s)." > /dev/stderr
+fi
 
 # create espeak "XML file". 
 # createSSML splits WORDLIST and generates SSML files.
@@ -101,4 +77,6 @@ python espeak2lexicon.py $WORDSLIST words-espeak > $OUTFILE
 # remove intermediate files.
 rm words-espeak out_*
 
-echo "Lexicon saved in file \"$OUTFILE\""
+if [ $OUTFILE != "/dev/stdout" ]; then
+  echo "Lexicon saved in file \"$OUTFILE\"" > /dev/stderr
+fi
